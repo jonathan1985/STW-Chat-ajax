598aa14e2b2e295b5268a5a3a61daa1686013872
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-03 13:03:13 +0000

    separated files for views and js

---
+++ public/chat.js
@@ -0,0 +1,14 @@
+$('#text').keypress(function(e){
+  if( e.keyCode==13 ){
+    $.get('/send',{text:$('#text').val()});
+    $('#text').val('');
+  }
+});
+var last = 0;
+setInterval(function(){
+  $.get('/update',{last:last},
+    function(response){
+      last = $('<p>').html(response).find('span').data('last');
+      $('#chat').append(response);
+    });
+  },1000);
