434bb404776c1641acab6d830221392f6487d819
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-04 09:56:00 +0000

    from here document to erb template: update

--- chat.rb
+++ chat.rb
@@ -1,5 +1,6 @@
 require 'sinatra' 
 require 'sinatra/reloader' if development?
+#require 'erb'
 #set :port, 3000
 #set :environment, :production
 
@@ -16,12 +17,23 @@ get '/send' do
 end
 
 get '/update' do
+  puts "update ***************"
+  p params
   updates = chat[params['last'].to_i..-1]
   last = %Q{<span data-last="#{chat.size}"></span>}
   if updates && updates.size>0
     puts "/update"
     p updates
-    response = updates.join('</br>') + "#{last}</br>"
+    #response = updates.join('</br>') + "#{last}</br>"
+
+    response = erb <<-'HTML', :locals => {:updates => updates, :last =>last } #, 0, '>' # trim mode
+        <% updates.each do |phrase| %>
+          <%= phrase %> <br />
+        <% end %>
+        <%= last %>
+    HTML
+    #response = template.result(binding())
+
     puts response
     response
   else
