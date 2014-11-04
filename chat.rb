7eca73ec2f20e799a26d02360c80781e32619966
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-04 10:28:24 +0000

    refactorizing. Cleaner now

--- chat.rb
+++ chat.rb
@@ -1,6 +1,5 @@
 require 'sinatra' 
 require 'sinatra/reloader' if development?
-#require 'erb'
 #set :port, 3000
 #set :environment, :production
 
@@ -9,34 +8,18 @@ chat = ['welcome..']
 get('/') { erb :index }
 
 get '/send' do
-  puts "send ...."
-  p params
   chat << "#{request.ip} : #{params['text']}"
-  p chat
   nil
 end
 
 get '/update' do
-  puts "update ***************"
-  p params
-  updates = chat[params['last'].to_i..-1]
-  last = %Q{<span data-last="#{chat.size}"></span>}
-  if updates && updates.size>0
-    puts "/update"
-    p updates
-    #response = updates.join('</br>') + "#{last}</br>"
+  @updates = chat[params['last'].to_i..-1] || []
 
-    response = erb <<-'HTML', :locals => {:updates => updates, :last =>last } #, 0, '>' # trim mode
-        <% updates.each do |phrase| %>
-          <%= phrase %> <br />
-        <% end %>
-        <%= last %>
-    HTML
-    #response = template.result(binding())
-
-    puts response
-    response
-  else
-    last
-  end
+  @last = chat.size
+  erb <<-'HTML', :layout => false
+      <% @updates.each do |phrase| %>
+        <%= phrase %> <br />
+      <% end %>
+      <span data-last="<%= @last %>"></span>
+  HTML
 end
--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,22 @@
-$('#text').keypress(function(e){
-  if( e.keyCode==13 ){
-    $.get('/send',{text:$('#text').val()});
-    $('#text').val('');
+$('#text').keypress(
+  function(e){
+    if( e.keyCode==13 ){
+      $.get('/send',{text:$('#text').val()});
+      $('#text').val('');
+    } 
   }
-});
-var last = 0;
-setInterval(function(){
-  $.get('/update',{last:last},
-    function(response){
-      last = $('<p>').html(response).find('span').data('last');
-      $('#chat').append(response);
-    });
-  },1000);
+);
+
+(function() {
+  var last = 0;
+  setInterval(
+    function(){
+      $.get('/update',{last:last},
+        function(response){
+          last = $('<p>').html(response).find('span').data('last');
+          $('#chat').append(response);
+        }
+      );
+    },
+  1000);
+})();
